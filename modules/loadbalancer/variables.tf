29f560859d29dc94c51dbaeae911c350879a2542
summu97 <135579543+summu97@users.noreply.github.com>
2024-12-27 15:38:24 +0530

    Update variables.tf

--- modules/loadbalancer/variables.tf
+++ modules/loadbalancer/variables.tf
@@ -1,8 +1,3 @@
-variable "project_id" {
-  description = "The GCP project ID"
-  type        = string
-}
-
 variable "zone" {
   description = "The zone for resources"
   type        = string
@@ -12,4 +7,10 @@ variable "app_name" {
   description = "The name of the application"
   type        = string
   default     = "app"
+}
+
+variable "instance_group_name" {
+  description = "The name of the group name"
+  type        = string
+  default     = "appdeploy-instance-group"
 }
