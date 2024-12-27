98470b7923143eb7d4e0c6484d07d0e4e4b28682
summu97 <135579543+summu97@users.noreply.github.com>
2024-12-27 12:23:45 +0530

    Create variables.tf

---
+++ modules/machines/variables.tf
@@ -0,0 +1,48 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The region where the VMs will be created"
+  type        = string
+}
+
+variable "zone" {
+  description = "The zone where the VMs will be created"
+  type        = string
+}
+
+variable "vm_machine_type" {
+  description = "Machine type for the VMs"
+  type        = string
+  default     = "e2-medium"
+}
+
+variable "app_vm1_name" {
+  description = "Name of the first application VM"
+  type        = string
+  default     = "app-vm1"
+}
+
+variable "app_vm2_name" {
+  description = "Name of the second application VM"
+  type        = string
+  default     = "app-vm2"
+}
+
+variable "db_vm_name" {
+  description = "Name of the database VM"
+  type        = string
+  default     = "db-vm"
+}
+
+variable "app_custom_data" {
+  description = "Custom data for application VMs"
+  type        = string
+}
+
+variable "db_custom_data" {
+  description = "Custom data for the database VM"
+  type        = string
+}
